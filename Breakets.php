630eb795369785baf4833df4845e5ea90fb739e5
Damir Fattahov <post@damir.in>
2015-04-20 16:42:26 +0500

    Initial commit

---
+++ Breakets.php
@@ -0,0 +1,27 @@
+<?php
+
+const SUCCESS_MSG = 'ОК';
+const ERROR_MSG   = 'Ошибка!';
+
+$expression = '{{[[{}]]]}';
+$count      = strlen($expression);
+
+if (strlen($expression) % 2 === 0) {
+	$items = str_split($expression);
+
+	foreach ($items as $key => $val) {
+		$symbol = $items[$count - $key - 1];
+
+		if (($val === '{' && $symbol === '}') || ($val === '[' && $symbol === ']')) {
+			addToLog($key, SUCCESS_MSG);
+		} else {
+			addToLog($key, ERROR_MSG);
+		}
+	}
+} else {
+	throw new Exception('Выражение содержит ошибку. Проверьте строку.');
+}
+
+function addToLog($key, $type) {
+	echo 'Символ номер: ' . $key . " - $type\n";
+}
