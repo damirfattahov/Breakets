544e9a813d5f45123f982aa48360c87c03197610
Damir Fattahov <post@damir.in>
2015-04-23 12:30:59 +0500

    Поправил баг с обходом всех символов;

--- Breakets.php
+++ Breakets.php
@@ -3,19 +3,24 @@
 const SUCCESS_MSG = 'ОК';
 const ERROR_MSG   = 'Ошибка!';
 
-$expression = '{{[[{}]]]}';
+$expression = '{{[[{{}]]]]}';
 $count      = strlen($expression);
 
 if (strlen($expression) % 2 === 0) {
+	$i = 0;
 	$items = str_split($expression);
 
 	foreach ($items as $key => $val) {
-		$symbol = $items[$count - $key - 1];
+		if ($i !== strlen($expression) / 2) {
+			$symbol = $items[$count - $key - 1];
 
-		if (($val === '{' && $symbol === '}') || ($val === '[' && $symbol === ']')) {
-			addToLog($key, SUCCESS_MSG);
-		} else {
-			addToLog($key, ERROR_MSG);
+			if (($val === '{' && $symbol === '}') || ($val === '[' && $symbol === ']')) {
+				addToLog($key, SUCCESS_MSG);
+			} else {
+				addToLog($key, ERROR_MSG);
+			}
+
+			$i++;
 		}
 	}
 } else {
